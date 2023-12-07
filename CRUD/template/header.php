5c2a4e021bd20abdc3b1d9b1dc8645e743aba564
jocelyn3011 <153086055+jocelyn3011@users.noreply.github.com>
2023-12-07 03:40:38 -0600

    Update header.php

--- CRUD/template/header.php
+++ CRUD/template/header.php
@@ -1,37 +1,13 @@
-<?php $url_base="http://localhost/CRUD/"; ?>
+<?php
+    $id=$_GET['id'];
+    include("conexion.php");
 
+    $sql="delete from alumnos where id='".$id."'";
+    $resultado=mysqli_query($conexion,$sql);
 
-<!doctype html>
-<html lang="en">
-    <head>
-        <title>CRUD</title>
-
-        <meta charset="utf-8" />
-        <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
-
-
-        <link
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-            rel="stylesheet"
-            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-            crossorigin="anonymous">
-    </head>
-
-    <body>
-        <nav class="navbar navbar-expand navbar-dark bg-dark">
-            <ul class="nav navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link active" href="#" aria-current="page">CRUD<span class="visually-hidden">(current)</span></a
-                    >
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?php echo $url_base;?>modulos/contacto/">Contactos</a>
-                </li>
-            </ul>
-        </nav>
-    <main class="container">
-    <br>
-    </html>
+    if($resultado){
+        echo "<script language='JavaScript'>alert('Los datos se eliminaron correctamente de la BD'); location.assign('index.php');</script>";
+    }else{
+        echo "<script language='JavaScript'>alert('Los datos se eliminaron correctamente de la BD'); location.assign('index.php');</script>";
+    }
+    ?>
